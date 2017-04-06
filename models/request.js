371af72431853a65abbe99919fc8703acb53ce54
Lorenzo Engelen <lorenzoengelen@gmail.com>
2017-04-06 15:47:13 +0200

    (setup) created model for a new request

---
+++ models/request.js
@@ -0,0 +1,17 @@
+var db = require('../db');
+
+exports.create = function(request, cb) {
+  // var request = {
+  //   id: id,
+  //   timestamp: time,
+  //   longitude: longitude,
+  //   latitude: latitude
+  // };
+
+  // db.save(request, cb);
+  cb('no error');
+};
+
+exports.update = function(id, info, cb) {
+
+};
