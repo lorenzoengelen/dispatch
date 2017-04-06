794af160616fdf11b5d4c4c00e8bf16c2cae29fb
Lorenzo Engelen <lorenzoengelen@gmail.com>
2017-04-06 12:34:09 +0200

    (feat) mongo error message

--- db/index.js
+++ db/index.js
@@ -1,6 +1,13 @@
 var mongoose = require('mongoose');
 var mongoUri = 'mongodb://localhost/dispatch';
 
-var db = mongoose.connect(mongoUri);
+var db = mongoose.connect(mongoUri, function(err) {
+  if (err) {
+    console.log('Unable to connect to mongo', err);
+    process.exit(1);
+  } else {
+    console.log('Connected to mongo uri', mongoUri);
+  }
+});
 
 module.exports = db;
