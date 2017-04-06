b94d934861bd4367ec3f12154c0f3c74f40bfcea
Lorenzo Engelen <lorenzoengelen@gmail.com>
2017-04-06 15:47:38 +0200

    (setup) test setup

--- controllers/requests.js
+++ controllers/requests.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-// var Request = require('../models/request');
+var Request = require('../models/request');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -10,6 +10,16 @@ router.get('/', function(req, res, next) {
 
 router.get('/new', function(req, res) {
   res.send('this is the requests/new route');
+});
+
+router.post('/new', function(req, res) {
+  var body = req.body;
+
+  console.log(Request);
+
+  Request.create(body, function(err) {
+    res.send(err);
+  });
 });
 
 module.exports = router;
