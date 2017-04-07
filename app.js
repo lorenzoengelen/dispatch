e04b8db33c4bdffcbc492b9e21f4573ff5b77e9d
Lorenzo Engelen <lorenzoengelen@gmail.com>
2017-04-07 15:57:53 +0200

    (setup) basic setup to use cluster of workers

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
+const cluster = require('cluster');
 const http = require('http');
+const numCPUs = require('os').cpus().length;
+
 const express = require('express');
 const path = require('path');
 const logger = require('morgan');
@@ -8,24 +11,47 @@ const bodyParser = require('body-parser');
 const middlewares = require('./middlewares/index');
 const routes = require('./controllers/index');
 
-const app = express();
+let server;
 
-app.set('port', process.env.PORT || 3000);
+if (cluster.isMaster) {
+  
+  console.log(`Master ${process.pid} is running`);
+  // fork workers
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
 
-// middlewares
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+  cluster.on('online', (worker) => {
+    console.log(`Worker ${worker.process.pid} is online`);
+  });
 
-app.use(middlewares);
-// controllers
-app.use('/', routes);
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`Worker ${worker.process.pid} died with code: ${code}, and signal: ${signal}`);
+    console.log('Starting a new worker');
+    cluster.fork();
+  });
 
-const server = http.createServer(app);
-server.listen(app.get('port'), () => {
-  console.log(`Server listening on port ${app.get('port')}`);
-});
+} else {
+
+  const app = express();
+  app.set('port', process.env.PORT || 3000);
+
+  // middlewares
+  app.use(logger('dev'));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(cookieParser());
+  app.use(express.static(path.join(__dirname, 'public')));
+
+  app.use(middlewares);
+  // controllers
+  app.use('/', routes);
+
+  server = http.createServer(app);
+  server.listen(app.get('port'), () => {
+    console.log(`Worker ${process.pid} listening on port ${app.get('port')}`);
+  });
+  
+}
 
 module.exports = server;
