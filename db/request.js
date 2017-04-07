3be4dd2436236be483aea1e84c2c127d0a3b3ccd
Lorenzo Engelen <lorenzoengelen@gmail.com>
2017-04-07 13:52:36 +0200

    (feat) updated schema for request minimum information needed

--- db/request.js
+++ db/request.js
@@ -4,7 +4,16 @@ const db = require('./index');
 mongoose.Promise = global.Promise;
 
 const requestSchema = new mongoose.Schema({
-  type: String
+  type: { type: String, required: true },
+  updated: { type: Date, default: Date.now },
+  location: {
+    longitude: { type: Number, required: true },
+    latitude: { type: Number, required: true }
+  },
+  timeWindow: {
+    lowerBound: { type: Number, required: true },
+    upperBound: { type: Number, required: true }
+  }
 });
 
 const Request = db.model('Request', requestSchema);
