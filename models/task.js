9a0dd72c8c18305f71e61b0cf37bd430dc45bb76
Lorenzo Engelen <lorenzoengelen@gmail.com>
2017-04-10 16:27:25 +0200

    (refac) refactored requests for tasks in model

--- models/task.js
+++ models/task.js
@@ -1,49 +1,49 @@
-const db = require('../db/request');
+const db = require('../db/task');
 
 exports.create = (body, cb) => {
   const {type, location, timeWindow} = body;
-  db.create({type, location, timeWindow}, (err, request) => {
+  db.create({type, location, timeWindow}, (err, task) => {
     console.log(err);
     if (err) return cb(err);
-    cb(null, request);
+    cb(null, task);
   });
 };
 
 exports.readAll = cb => {
-  db.find({}).exec((err, requests) => {
+  db.find({}).exec((err, tasks) => {
     if (err) return cb(err);
-    cb(null, requests);
+    cb(null, tasks);
   });
 };
 
 exports.read = (id, cb) => {
-  db.find({_id: id}).exec((err, request) => {
+  db.find({_id: id}).exec((err, task) => {
     if (err) return cb(err);
-    cb(null, request);
+    cb(null, task);
   });
 };
 
 exports.update = (id, body, cb) => {
   const {type} = body;
-  db.findOneAndUpdate({_id: id}, {type: type}, (err, request) => {
+  db.findOneAndUpdate({_id: id}, {type: type}, (err, task) => {
     if (err) return cb(err);
-    cb(null, request);
+    cb(null, task);
   });
 };
 
 exports.deleteAll = cb => {
-  db.find({}).exec((err, requests) => {
+  db.find({}).exec((err, tasks) => {
     if (err) return cb(err);
     db.remove(err => {
       if (err) return cb(err);
-      cb(null, requests);
+      cb(null, tasks);
     });
   });
 };
 
 exports.delete = (id, cb) => {
-  db.findOneAndRemove({_id: id}, (err, request) => {
+  db.findOneAndRemove({_id: id}, (err, task) => {
     if (err) return cb(err);
-    cb(null, request);
+    cb(null, task);
   });
 };
