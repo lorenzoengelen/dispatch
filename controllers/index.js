799729a957f1c585e75448bae9e57a526000e028
Lorenzo Engelen <lorenzoengelen@gmail.com>
2017-04-11 08:42:26 +0200

    (feat) created depot route

--- controllers/index.js
+++ controllers/index.js
@@ -3,9 +3,11 @@ const router = express.Router();
 
 const users = require('./users');
 const tasks = require('./tasks');
+const depots = require('./depots');
 
 router.use('/users', users);
 router.use('/tasks', tasks);
+router.use('/depots', depots);
 
 /* GET users listing. */
 router.get('/', (req, res, next) => {
