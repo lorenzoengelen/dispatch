d3c2724ff8da3031da80460a04216fdc3887a4ea
Lorenzo Engelen <lorenzoengelen@gmail.com>
2017-04-14 16:08:18 +0200

    (feat) update function for route class

--- helpers/pdp/classes/Route.js
+++ helpers/pdp/classes/Route.js
@@ -5,7 +5,7 @@ class Route {
     this.path = []; // node ids in path
     this.orders = []; // order ids associated with nodes
     
-    this.updated = false;
+    this.updated = true;
     
     this.D = 0; // route duration
     this.TWV = 0; // time-window violations
@@ -17,12 +17,37 @@ class Route {
   }
 
   update() {
+    let D = 0;
+    let TWV = 0;
+    let CV = 0;
+
+    const w1 = 0.201;
+    const w2 = 0.7;
+    const w3 = 0.099;
+
+    let q = 0; // used capacity
+
+    for (let i = 0, len = this.path.length; i < len; i++) {
+      // do some stuff here
+    }
+
+    this.D = D;
+    this.TWV = TWV;
+    this.CV = CV;
+
+
+    this.cost = w1 * D + w2 * TWV + w3 * CV;
+
+    this.updated = false;
   }
 
   testPath(path) {
   }
 
   getCost() {
+    if (this.updated) {
+
+    }
   }
 
   addOrder(order) {
