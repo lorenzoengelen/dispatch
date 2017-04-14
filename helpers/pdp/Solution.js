8c476e2985d4648b985b572248f2da5423fffc13
Lorenzo Engelen <lorenzoengelen@gmail.com>
2017-04-14 15:40:04 +0200

    (feat) created Solution class

---
+++ helpers/pdp/Solution.js
@@ -0,0 +1,14 @@
+class Solution {
+  constructor(problem) {
+    this.problem = problem;
+    this.route = [];
+    this.totalDistance = 0;
+    this.totalCost = 0;
+  }
+
+  initialConstruction() {
+    console.log('hello');
+  }
+};
+
+module.exports = Solution;
