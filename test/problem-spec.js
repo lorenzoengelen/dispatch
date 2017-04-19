44bdcb6d825e0bc13c635f4293ecea1fb76b9e74
Lorenzo Engelen <lorenzoengelen@gmail.com>
2017-04-19 12:40:44 +0200

    (test) test problem file

--- test/problem-spec.js
+++ test/problem-spec.js
@@ -4,7 +4,8 @@ const expect = chai.expect;
 const should = chai.should();
 
 const Problem = require('../helpers/pdp/classes/Problem');
-
+const Node = require('../helpers/pdp/classes/Node');
+const json = require('./problem-spec.json');
 
 describe('Problem Class', () => {
   
@@ -29,13 +30,36 @@ describe('Problem Class', () => {
   });
 
   describe('loadProblem function', () => {
+
+    let problem = new Problem();
+
     it('should exist', () => {
       expect(problem.loadProblem()).be.function; 
     });
 
-    it('should load a json file', () => {
+    it('should parse K of json file', () => {
+      problem.loadProblem(json);
+      expect(problem.K).to.equal(json.K);
+    })
 
+    it('should parse Q of json file', () => {
+      expect(problem.Q).to.equal(json.Q);
+    })
+
+    it('should parse nodes of json file', () => {
+      expect(problem.N).to.have.lengthOf(json.N.length);
+      expect(problem.N[0]).to.be.an.instanceof(Node);
+      expect(problem.N[0].nid, 'node id').to.equal(json.N[0].nid);
+      expect(problem.N[0].x, 'x location').to.equal(json.N[0].x);
+      expect(problem.N[0].y, 'y location').to.equal(json.N[0].y);
+      expect(problem.N[0].demand, 'demand quantity').to.equal(json.N[0].demand);
+      expect(problem.N[0].twOpen, 'earliest time window').to.equal(json.N[0].twOpen);
+      expect(problem.N[0].twClose, 'latest time window').to.equal(json.N[0].twClose);
+      expect(problem.N[0].service, 'service time').to.equal(json.N[0].service);
+      expect(problem.N[0].pid, 'pickup id').to.equal(json.N[0].pid);
+      expect(problem.N[0].did, 'delivery id').to.equal(json.N[0].did);
     });
-  });
 
-});
+  }); // loadProblem function
+
+}); // Problem Class
