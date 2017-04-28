fbeb2fb75db4d2c64f71440ed3feb6f3bbaf3f12
Lorenzo Engelen <lorenzoengelen@gmail.com>
2017-04-28 13:28:33 +0200

    (feat) created Problem and Solution class

---
+++ helpers/vrp/classes/Problem.js
@@ -0,0 +1,9 @@
+class Problem {
+  constructor() {
+    this.N = []; // array of nodes
+    this.O = []; // array of orders
+  }
+
+}
+
+module.exports = Problem;
---
+++ helpers/vrp/classes/Solution.js
@@ -0,0 +1,10 @@
+class Solution {
+  constructor(problem) {
+    this.problem = problem;
+  }
+
+  initialSolution() {
+  }
+};
+
+module.exports = Solution;
